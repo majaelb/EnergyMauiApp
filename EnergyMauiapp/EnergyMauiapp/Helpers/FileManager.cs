d23448bba44e1973d082eaf31aa93252247aee8e
majaelb <115992540+majaelb@users.noreply.github.com>
2023-03-16 15:40:19 +0100

    Energiappen 1.0 är klar!

--- EnergyMauiapp/EnergyMauiapp/Helpers/FileManager.cs
+++ EnergyMauiapp/EnergyMauiapp/Helpers/FileManager.cs
@@ -9,6 +9,24 @@ using Microsoft.Maui.Storage;
 
 namespace EnergyMauiapp.Helpers
 {
+    //Metoderna i denna klass motsvarar en "facade" i mitt program.
+    //Jag letade länge efter ett designpattern utan att hitta något som jag tyckte passade för att läsa och skriva till fil, som mitt program är uppbyggt av
+    //och där ett design pattern kunde ha varit till hjälp. Jag förstår samtidigt att det kanske visst finns något som kunde ha passat, men med de kunskaper 
+    //jag besitter i området idag så kunde jag inte hitta något som jag kunde se framför mig användas i mitt projekt. Jag tror dessutom att om jag hittat ett
+    //pattern hade behövt bestämma mig för ett pattern att följa från start, och inte försöka lägga till något så pass sent i projektets gång då jag hade behövt
+    //bygga om så stora delar av koden att tiden inte hade räckt till.
+    //Om jag förstått tanken med facade rätt, så är det att koka ner bakomliggande funktioner till en enda enkel sådan och då jag läser och skriver till fil
+    //på så pass många ställen i programmet så hade jag inte kunnat ha en enda sida för detta, utan det hade blivit flera Facader varpå det känns som att idén
+    //med designmönstret går förlorad. Singleton använder jag däremot och har stor nytta av för att hålla reda på data under programmets gång.
+    //Tanken med min app är ju att den ska användas av och vara ett fungerande hjälpmedel för hjärntrötta. Av den anledningen valde jag bort inloggning, id:n
+    //och anslutning till databaser, så att användaren bara ska kunna fokusera på att använda själva appen utan att registrera sig och hålla rätt på inloggnings-
+    //uppgifter. Hade jag haft någon form av registrering hade dock en implementering av Facade känts som ett bra alternativ. Men återigen så är tanken med appen
+    //att den ska vara så enkel det bara går att använda. Användaren ska navigera enbart med hjälp av knapptryckningar för att minimera riskerna för att göra fel
+    //och tappa bort sig i programmet. 
+    //Metoderna i denna klass sköter all typ av filhantering som jag behöver. Jag har gjort de flesta metoderna asynkrona och generiska för att de ska vara smidiga att
+    //använda till alla typer av object som behöver hämtas eller skrivas. Metoden som hämtar en lista OnAppearing fick jag dock se mig besegrad av pga buggen som
+    //duplicerade första posten i listan, och fick göra en synkron metod för att kringgå problemet. 
+
     internal class FileManager
     {
         public static bool FindExistingFile(string fileName)
@@ -46,7 +64,6 @@ namespace EnergyMauiapp.Helpers
             return path;
         }
       
-        //TODO: Denna klass och metoder motsvarar min facade?
         public static async Task<T> GetObjectFromTxt<T>(string fileName)
         {
             string path = GetFilePath(fileName);
