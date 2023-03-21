92caa20384ad897770e52e132cd15cea03576648
majaelb <115992540+majaelb@users.noreply.github.com>
2023-03-21 13:52:04 +0100

    Set maximum windowsize

--- EnergyMauiapp/EnergyMauiapp/App.xaml.cs
+++ EnergyMauiapp/EnergyMauiapp/App.xaml.cs
@@ -2,10 +2,28 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+    }
+
+    //Sätter fönstret till en fast max-storlek
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 900;
+        const int newHeight = 1050;
+
+        window.MaximumHeight = newHeight;
+        window.MaximumHeight = newHeight;
+
+        window.MaximumWidth = newWidth;
+        window.MaximumWidth = newWidth;
+
+        return window;
+    }
 }
+
